0b23940db9c30baf68a36510187fe72a691300fb
Albert221 <w.albert221@gmail.com>
2018-11-26 12:07:42 +0100

    Add fully working, readable DosingSection

--- lib/ui/medicine/dosing_section.dart
+++ lib/ui/medicine/dosing_section.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lek_bierz/main.dart';
 import 'package:lek_bierz/redux/state.dart';
 import 'package:lek_bierz/ui/common/list_header.dart';
 
@@ -6,42 +7,48 @@ class DosingSection extends StatelessWidget {
   final Dosing dosing;
   final Function onAddDosingTap;
 
-  DosingSection(this.dosing, {Key key, this.onAddDosingTap})
-      : super(key: key);
+  DosingSection(this.dosing, {Key key, this.onAddDosingTap}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    var children = <Widget>[];
+    children
+      ..add(Padding(
+          padding: EdgeInsets.only(left: 16.0, right: 8.0),
+          child: Chip(
+              label: Text(_getFrequencyDescription(dosing.frequency)),
+              backgroundColor: MyApp.lightGrayColor)))
+      ..addAll(_buildDoseTimes(dosing.times.toList()));
+
     return Padding(
         padding: EdgeInsets.only(bottom: 16.0),
-        child: Column(
-          children: [
-            ListHeader('Dawkowanie'),
-            dosing != null
-                ? Row(
-                    children: [
-                      Chip(
-                          label:
-                              Text(_getFrequencyDescription(dosing.frequency))),
-                      SizedBox(width: 8.0),
-                      Chip(label: Text('Rano')),
-                      SizedBox(width: 8.0),
-                      Chip(label: Text('Po południu')),
-                      SizedBox(width: 8.0),
-                      Chip(label: Text('Wieczorem')),
-                    ],
-                  )
-                : FlatButton(
-                    child: Row(mainAxisSize: MainAxisSize.min, children: [
-                      Icon(Icons.add_alert, color: Colors.white, size: 20.0),
-                      SizedBox(width: 8.0),
-                      Text('USTAW DAWKOWANIE',
-                          style: TextStyle(color: Colors.white))
-                    ]),
-                    color: Theme.of(context).primaryColor,
-                    onPressed: () => onAddDosingTap(),
-                  ),
-          ],
-        ));
+        child: Column(mainAxisSize: MainAxisSize.min, children: [
+          Padding(
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+              child: ListHeader('Dawkowanie')),
+          dosing != null
+              ? SizedBox(
+                  height: 48.0,
+                  child: ListView(
+                      scrollDirection: Axis.horizontal, children: children))
+              : FlatButton(
+                  child: Row(mainAxisSize: MainAxisSize.min, children: [
+                    Icon(Icons.add_alert, color: Colors.white, size: 20.0),
+                    SizedBox(width: 8.0),
+                    Text('USTAW DAWKOWANIE',
+                        style: TextStyle(color: Colors.white))
+                  ]),
+                  color: Theme.of(context).primaryColor,
+                  onPressed: () => onAddDosingTap())
+        ]));
+  }
+
+  List<Widget> _buildDoseTimes(List<DoseTime> doseTimes) {
+    return doseTimes
+        .map((time) => Padding(
+            padding: EdgeInsets.only(right: 8.0),
+            child: Chip(label: Text(_getTimeDescription(time)))))
+        .toList();
   }
 
   String _getFrequencyDescription(DosingFrequency frequency) {
@@ -58,6 +65,29 @@ class DosingSection extends StatelessWidget {
         return 'Co pięć dni';
       case DosingFrequency.everyWeek:
         return 'Co tydzień';
+    }
+
+    return '';
+  }
+
+  String _getTimeDescription(DoseTime time) {
+    switch (time) {
+      case DoseTime.morning:
+        return 'Rano';
+      case DoseTime.afterBreakfast:
+        return 'Po śniadaniu';
+      case DoseTime.beforeNoon:
+        return 'Przed południem';
+      case DoseTime.noon:
+        return 'W południe';
+      case DoseTime.afterLunch:
+        return 'Po obiedzie';
+      case DoseTime.beforeDinner:
+        return 'Przed kolacją';
+      case DoseTime.afterDinner:
+        return 'Po kolacji';
+      case DoseTime.beforeSleep:
+        return 'Przed snem';
     }
 
     return '';
